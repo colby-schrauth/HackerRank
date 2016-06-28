edbbc20d8c23f6a2fe1b25cd68773cad329cf801
Colby Schrauth <colby.schrauth@gmail.com>
2016-06-27 21:28:14 -0700

    Update pangrams.py

--- Algorithms/Strings/pangrams.py
+++ Algorithms/Strings/pangrams.py
@@ -1,6 +1,12 @@
+# Create variable 'x'
+# Accept user input as a string and convert all characters to lowercase
 x = str(raw_input().lower())
+
+# Create variable 'y'
+# Convert user inputted string ('x') to a list, separating each character, and remove all duplicate characters with the function
 y = list(set(x))
 
+# Including the space character (' '), count the length of our unique list ('y') and print results
 if len(y) == 27:
     print 'pangram'
 else:
